24fd47b1c9b87035c5779f29c9c93db294b7ecaf
Kshitij Mishra <kshitijmishra1302@gmail.com>
2024-12-20 17:32:05 +0530

    updated

--- src/components/AddCompanyForm.jsx
+++ src/components/AddCompanyForm.jsx
@@ -35,6 +35,7 @@ export default function AddCompanyForm() {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`, // Add token to Authorization header
         },
+        mode: "no-cors",
         body: JSON.stringify(jobData),
       });
 
